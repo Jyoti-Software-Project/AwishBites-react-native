4d1c9def0532ebe0afa5e088823919362294b970
Jyoti-Software-Project <jyotiprakashmal104@gmail.com>
2024-12-01 20:30:35 +0530

    update need prop type

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
-import JyotiBites from './src/JyotiBites';
+import { SafeAreaView, StatusBar, StyleSheet, View, Text } from 'react-native';
+// import JyotiBites from './src/JyotiBites';
 
 export default function App() {
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar animated={true} backgroundColor="#007AFF" />
             <View style={styles.appContainer}>
-                <JyotiBites />
-                <View>Demo</View>
+                {/* <JyotiBites /> */}
+                <Text>Demo</Text>
             </View>
         </SafeAreaView>
     );
